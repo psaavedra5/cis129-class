106e600709cb46f93d3e509fbf8117c457054c5a
Juan Pedro Saavedra <181182908+psaavedra5@users.noreply.github.com>
2024-09-23 09:46:32 -0700

    Update cis129_lab03_coffeeShop.py
    Added cheesecake and chocomilk

--- cis129_lab03_coffeeShop.py
+++ cis129_lab03_coffeeShop.py
@@ -5,30 +5,30 @@ print ('My Coffee and Muffin Shop')
 
 Muffin = 4.00 
 Coffee = 5.00
+Cheesecake = 12.00
+Chocomilk = 2.00
+## Here is to declare the prices of the articles
 
-# Declare()   ##Save for the two new items 
-# Declare()
-## Here is to declare the variable to use in the program
 print ('Number of coffees bought?')
 number_coffees = int(input ())
 print('Number of muffins bought ?')
 number_muffins = int(input())
-	# Print('Number of cheesecakes bought ?')
-	# input(number_cheesecakes)
-	# Print('Number of chocomilks bought ? ')
-	# input (number_chocomilks)
+	Print('Number of cheesecakes bought ?')
+	number_cheesecakes = int(input())
+	Print('Number of chocomilks bought ? ')
+	 number_chocolatemilks = int(input())
 ## Here is asking the user how many of each item would it like 
 
 print('*************************************')
 
-Totalcoffes= number_coffees * Coffee
-Totalmuffins= number_muffins * Muffin
-#Totalcheesecakes=
-#Totalchocomilks=
+Totalcoffes = number_coffees * Coffee
+Totalmuffins = number_muffins * Muffin
+Totalcheesecakes = number_cheesecakes * Cheesecake
+Totalchocomilks = number_chocomilks * Chocomilk
 ##Here is each total individully with out taxes
 
-Subtotal= (number_coffees * Coffee)+(number_muffins*Muffin)
-#Subtotal= (number_cheesecakes * Cheesecake) + (number_chocomilks * Chocomilk )
+Subtotal= (number_coffees * Coffee)+(number_muffins*Muffin) \
+ (number_cheesecakes * Cheesecake) + (number_chocomilks * Chocomilk )
 ## Here is the subtotal to calculate the tax out of that 
 
 tax=0.06 * Subtotal
@@ -39,8 +39,11 @@ print('*************************************')
 print ('My Coffe and Muffin Shop Receipt')
 display (number_coffees,'Coffee at $5 each:',Totalcoffes)
 display (number_muffins,'Muffins at  $4 each',Totalmuffins)
+display (number_cheesecakes, 'Cheesecake at $12 each:', Totalcheesecakes)
+display (number_chocomilks, 'Chocomilk at $2 each:', Totalchocomilks)
 display('6% tax:',tax)
 print('-----------')
 display('Total:',Total)
+print('Thank you so much to play my gameee ... sorry mario64 fan, Thanks for your visit  :D ')
 
 # Here is the e-receipt for what they buy
